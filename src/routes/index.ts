3d42760f74adf8823f345995e0882416abb283b3
Piscinao <henhvj@gmail.com>
2020-09-07 23:08:35 -0300

    Appointments

---
+++ src/routes/index.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import appointsmentsRouter from './appointments.routes';
+const routes = Router();
+
+routes.use('/appointments', appointsmentsRouter);
+
+export default routes;
