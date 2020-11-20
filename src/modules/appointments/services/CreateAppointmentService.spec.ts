0fc26e375d937013b2e40a9ac45d888aa5cbb9c2
Piscinao <henhvj@gmail.com>
2020-11-20 17:54:33 -0300

    Finalizando teste de appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,10 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
+    // salva na memoria pois chama o repositorio fake diretamente
     const fakeAppointmentRepository = new FakeAppointmentsRepository();
     const createAppointment = new CreateAppointmentService(
       fakeAppointmentRepository,
@@ -17,7 +19,27 @@ describe('CreateAppointment', () => {
     expect(appointment.provider_id).toBe('123123123');
   });
 
-  it('should not be able to two appointments on the same time', () => {});
+  it('should not be able to two appointments on the same time', async () => {
+    // salva na memoria pois chama o repositorio fake diretamente
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123123123',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
 // test('sum two numbers', () => {
 //   expect(1 + 2).toBe(3);
