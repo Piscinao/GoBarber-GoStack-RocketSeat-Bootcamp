ee00a8ca078faeff9baeb7e2ec172067f47108fd
Piscinao <henhvj@gmail.com>
2020-11-20 21:03:51 -0300

    Finalizando testes de autenticacao

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserService from './AuthenticateUserService';
@@ -33,5 +34,52 @@ describe('AuthenticateUser', () => {
     expect(response).toHaveProperty('token');
     // a resposta seja igual ao usuario criado
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    // salva na memoria pois chama o repositorio fake diretamente
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    // espera que resposta contenha o token
+    expect(
+      authenticateUser.execute({
+        email: 'john@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+    // a resposta seja igual ao usuario criado
+  });
+
+  it('should be able to authenticate with wrong password', async () => {
+    // salva na memoria pois chama o repositorio fake diretamente
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'john@example.com',
+      password: '123456',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'john@example.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
