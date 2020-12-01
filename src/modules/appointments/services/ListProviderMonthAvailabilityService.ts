b9c287e935a9c80d8c9b4a6738a65fbac5f365a6
Piscinao <henhvj@gmail.com>
2020-11-30 22:29:32 -0300

    Listar dias disponiveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -25,14 +26,37 @@ class ListProviderMonthAvailabilityService {
     year,
     month,
   }: IRequest): Promise<IResponse> {
-    const appointments = this.appointmentsRepository.findAllInMonthProvider({
-      provider_id,
-      year,
-      month,
+    const appointments = await this.appointmentsRepository.findAllInMonthProvider(
+      {
+        provider_id,
+        year,
+        month,
+      },
+    );
+
+    // numero de dias no mês e ano
+    const numberOfDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    // array [1, 2, 3, 4, 5, 6, 7, 8, 9] até o dia do mês
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    // retorna todos os agendamentos de um dia específico
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
     });
 
-    console.log(appointments);
-    return [{ day: 1, available: false }];
+    console.log(eachDayArray);
+    return availability;
   }
 }
 
