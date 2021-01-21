11b928c07a907500f09075b74e9dacc6cbb3e6ca
Piscinao <henhvj@gmail.com>
2021-01-20 22:02:27 -0300

    Config upload AWS - S3

--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,16 +1,20 @@
 import { container } from 'tsyringe';
+import uploadConfig from '@config/upload';
 
 import IStorageProvider from './models/IStorageProvider';
 
 import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
   disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 // quando usa o registersingleton ele não utiliza o constructor cda classe e tava criando de uma utra forma
+// container resolve ele q faz a injeção de dependcia ele q vai ver se o constructor tem os injectable
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
-  // container resolve ele q faz a injeção de dependcia ele q vai ver se o constructor tem os injectable
-  providers.disk,
+  providers[uploadConfig.driver],
 );
