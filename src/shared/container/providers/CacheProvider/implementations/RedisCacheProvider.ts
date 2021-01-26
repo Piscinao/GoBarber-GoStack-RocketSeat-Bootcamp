d9fc1ae7039973099e456bbdccdd4532d9512d0f
Piscinao <henhvj@gmail.com>
2021-01-25 23:30:56 -0300

    Invalidando o cache

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -27,4 +27,17 @@ export default class RedisCacheProvider implements ICacheProvider {
   }
 
   public async invalidate(key: string): Promise<void> {}
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    // busca todas as chaves que começam com providerlist e tenham : e alguma coisa
+    const keys = await this.client.keys(`${prefix}:*`);
+    // multiplas operações ao mesmo tempo, não bloqueia a execução do restante REDIS
+    const pipeline = this.client.pipeline();
+
+    keys.forEach(key => {
+      pipeline.del(key);
+    });
+
+    await pipeline.exec();
+  }
 }
